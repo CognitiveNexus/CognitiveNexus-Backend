d01166a1753f82e6be2746a173ec230b17b4f128
BobLiu <bobliu@bobliu.tech>
2025-04-03 13:39:11 +0800

    feat: add support to user codes

--- app/controllers/AIController.php
+++ app/controllers/AIController.php
@@ -3,6 +3,7 @@
 class AIController {
     public static function askAI($model) {
         $messages = Flight::request()->data->messages;
+        $code = Flight::request()->data->code;
         if (!$messages) {
             Flight::jsonHalt(['error' => '未提供聊天记录'], 406);
         }
@@ -22,21 +23,53 @@ class AIController {
             Flight::jsonHalt(['error' => "无法调用模型 {$model}"], 406);
         }
 
-        function handleChunk($chunk) {
-            $reply = ['success' => true];
-            if (isset($chunk->choices[0]->delta)) {
-                $delta = $chunk->choices[0]->delta;
-                if (isset($delta->reasoning_content)) {
-                    $reply['reasoning_content'] = $delta->reasoning_content;
+        try {
+            while (true) {
+                $result = self::sendRequest($host, $key, $model, $messages);
+                if ($result['finish'] != 'tool_calls') {
+                    break;
                 }
-                if (isset($delta->content)) {
-                    $reply['content'] = $delta->content;
+                $messages[] = $result;
+                foreach ($result['tool_calls'] as $toolCall) {
+                    if ($toolCall['function']['name'] == 'getUserCode') {
+                        $toolContent = $code ?? '用户没有提供代码';
+                    } else {
+                        $toolContent = '未知调用';
+                    }
+                    $messages[] = [
+                        'role' => 'tool',
+                        'tool_call_id' => $toolCall['id'],
+                        'content' => $toolContent,
+                    ];
                 }
             }
-            echo json_encode($reply)."\n\n";
-            ob_flush();
-            flush();
+        } catch (Exception $err) {
+            Flight::jsonHalt(['error' => $err->getMessage()], 500);
+        } finally {
+            ob_end_flush();
         }
+    }
+
+    private static function sendRequest($host, $key, $model, $messages) {
+        $body = [
+            'model' => $model,
+            'messages' => [
+                ['role' => 'system', 'content' => $_ENV['AI_PROMPT']],
+                ...$messages,
+            ],
+            'tools' => [
+                [
+                    'type' => 'function',
+                    'function' => [
+                        'name' => 'getUserCode',
+                        'description' => '获取用户提供的代码',
+                    ]
+                ]
+            ],
+            'temperature' => 0,
+            'stream' => true,
+        ];
+        $result = [];
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "{$host}/chat/completions");
@@ -46,31 +79,9 @@ class AIController {
             "Authorization: Bearer {$key}",
             'Content-Type: application/json',
         ]);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
-            'model' => $model,
-            'messages' => [
-                ['role' => 'system', 'content' => $_ENV['AI_PROMPT']],
-                ...$messages,
-            ],
-            'temperature' => 0,
-            'stream' => true,
-        ]));
-        curl_setopt($ch, CURLOPT_WRITEFUNCTION, function ($ch, $data) {
-            $lines = explode("\n\n", trim($data));
-            foreach ($lines as $line) {
-                if (strpos($line, 'data: ') === 0) {
-                    $chunk = substr($line, 6);
-                    if ($chunk != '[DONE]') {
-                        $chunk = json_decode($chunk);
-                        if ($chunk) {
-                            handleChunk($chunk);
-                        }
-                    }
-                } else if (strlen(trim($line)) && strpos($line, ':') !== 0) {
-                    throw new Exception(trim($line));
-                }
-            }
-            return strlen($data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+        curl_setopt($ch, CURLOPT_WRITEFUNCTION, function ($ch, $data) use (&$result) {
+            return self::handleStream($data, $result);
         });
 
         try {
@@ -78,11 +89,64 @@ class AIController {
             if (curl_errno($ch)) {
                 throw new Exception(curl_error($ch));
             }
-        } catch (Exception $err) {
-            Flight::jsonHalt(['error' => $err->getMessage()], 500);
         } finally {
             curl_close($ch);
-            ob_end_flush();
+        }
+
+        return $result;
+    }
+
+    private static function handleStream($data, &$result) {
+        $lines = explode("\n\n", trim($data));
+        foreach ($lines as $line) {
+            if (!trim($line)) {
+                continue;
+            }
+            [$prefix, $chunk] = explode(':', trim($line), 2);
+            if (!$prefix) {
+                self::responseDelta([]);
+                continue;
+            } else if ($prefix != 'data') {
+                throw new Exception(trim($line));
+            }
+
+            if ($chunk == '[DONE]') {
+                continue;
+            }
+            $chunk = json_decode($chunk, true);
+            if ($chunk) {
+                $delta = $chunk['choices'][0]['delta'];
+                self::mergeDelta($result, $delta);
+                self::responseDelta($delta);
+                if ($chunk['choices'][0]['finish_reason']) {
+                    $result['finish'] = $chunk['choices'][0]['finish_reason'];
+                }
+            }
+        }
+        return strlen($data);
+    }
+
+    private static function responseDelta($delta) {
+        $reply = ['success' => true];
+        foreach (['reasoning_content', 'content'] as $key) {
+            if (array_key_exists($key, $delta)) {
+                $reply[$key] = $delta[$key];
+            }
+        }
+        echo json_encode($reply)."\n\n";
+        ob_flush();
+        flush();
+    }
+
+    private static function mergeDelta(&$origin, $delta) {
+        foreach ($delta as $key => &$value) {
+            if (is_array($value) && isset($origin[$key]) && is_array($origin[$key])) {
+                self::mergeDelta($origin[$key], $value);
+            } else if (is_string($value) && isset($origin[$key]) && is_string($origin[$key])) {
+                $origin[$key] .= $value;
+            } else {
+                $origin[$key] = $value;
+            }
         }
     }
 }
