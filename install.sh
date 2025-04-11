bf2ba34533d18091b47f08869177ae4cfe6b3cd6
BobLiu <bobliu@bobliu.tech>
2025-04-11 13:19:54 +0800

    feat: auto set password when installing db

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .env
-psql -h $DB_HOST -p $DB_PORT -U $DB_USERNAME -d $DB_NAME -f install.sql 
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USERNAME -d $DB_NAME -f install.sql 
